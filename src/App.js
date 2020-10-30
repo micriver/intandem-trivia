17d8b31d21ee2117b9cc826a0400fa3f127f16b2
Michael Rivera <mirivera@student.42.us.org>
2020-10-30 15:34:39 -0700

    accessed JSON object props, still have to modify presentation and stuff

--- src/App.js
+++ src/App.js
@@ -13,3 +13,26 @@ function App() {
 }
 
 export default App;
+
+/*
+
+Assumptions
+• • • • •
+
+[ ] A round of trivia has 10 Questions
+[ ] All questions are multiple-choice questions
+[ ] Your score does not need to update in real time
+[ ] Results can update on form submit, button click, or any interaction you choose
+[✓] We will provide you with the trivia data such as the questions, correct and incorrect answers via a JSON file.
+
+Acceptance Criteria
+• • • • • •
+[✓] A user can view questions.
+[ ] Questions with their multiple choice options must be displayed one at a time. Questions should not repeat in a round.
+[ ] A user can select only 1 answer out of the 4 possible answers.
+[ ] The correct answer must be revealed after a user has submitted their answer
+[ ] A user can see the score they received at the end of the round
+
+ctrl+k, shift+o,k for checkmark
+
+*/
--- src/components/QuestionCard/QuestionCard.js
+++ src/components/QuestionCard/QuestionCard.js
@@ -3,13 +3,20 @@ import styles from "./QuestionCard.module.css";
 import trivia from "../../assets/JSON/Apprentice_TandemFor400_Data.json";
 
 export default function QuestionCard() {
+  console.log(trivia[0]);
+
+  // mapping incorrect object property to a JSX variable
+  const listItems = Object.values(trivia[0].incorrect).map((incAnswers) => (
+    <li>{incAnswers}</li>
+  ));
+
   return (
     <div className={styles.container}>
       <div className={styles.card}>
         <div className={styles.question}>{trivia[0].question}</div>
         <div className={styles.answers}>
-          <p>{trivia[0].incorrect}</p>
-          <p>{trivia[0].correct}</p>
+          <div className='items'>{listItems}</div>
+          <li>{trivia[0].correct}</li>
         </div>
       </div>
     </div>
