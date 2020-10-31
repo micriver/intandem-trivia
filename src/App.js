f2183388803ccd6298a73accde2dd6dafd591f89
Michael Rivera <mirivera@student.42.us.org>
2020-10-30 19:07:57 -0700

    have props displayed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import QuestionCard from "./components/QuestionCard/QuestionCard";
+import trivia from "./assets/JSON/Apprentice_TandemFor400_Data.json";
 
 import styles from "./App.module.css";
 
 function App() {
+  // modify rounds to show different questions
+  const [round, setRound] = useState(1);
+  // you have to add logic for another question without repeats
+  // keep track of questions between rounds and display all of them between the two rounds
+  const randomNum = Math.floor(Math.random() * trivia.length);
+
+  console.log(randomNum);
+
+  // const randomQuestion = Object.values(trivia[randomNum])[0];
+
+  // console.log(randomQuestion);
+
+  const answerHandler = () => {
+    console.log("you're clicking the button!");
+  };
+
+  const answerHandler = () => {
+    console.log("you're clicking the button!");
+  };
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>inTandem Trivia!</h1>
-      <QuestionCard />
+      <h1 className={styles.title}>Trivia!</h1>
+      <QuestionCard
+        correct={Object.values(trivia[randomNum])[2]}
+        incorrect={Object.values(trivia[randomNum])[1]}
+        question={Object.values(trivia[randomNum])[0]}
+      />
     </div>
   );
 }
@@ -15,6 +39,10 @@ function App() {
 export default App;
 
 /*
+
+The JSON data is an array of objects. Each object has a question and answers as properties of that object
+
+You must randomly select a question from the JSON array on startup.
 
 Assumptions
 • • • • •
--- src/components/QuestionCard/QuestionCard.js
+++ src/components/QuestionCard/QuestionCard.js
@@ -2,21 +2,24 @@ import React from "react";
 import styles from "./QuestionCard.module.css";
 import trivia from "../../assets/JSON/Apprentice_TandemFor400_Data.json";
 
-export default function QuestionCard() {
-  console.log(trivia[0]);
-
+export default function QuestionCard({ question, incorrect, correct }) {
+  const answerHandler = () => {
+    console.log("you're clicking the button!");
+  };
   // mapping incorrect object property to a JSX variable
-  const listItems = Object.values(trivia[0].incorrect).map((incAnswers) => (
-    <li>{incAnswers}</li>
+  const listItems = incorrect.map((incAnswers) => (
+    <li className={styles.items}>{incAnswers}</li>
   ));
 
   return (
     <div className={styles.container}>
       <div className={styles.card}>
-        <div className={styles.question}>{trivia[0].question}</div>
+        <div className={styles.question}>{question}</div>
         <div className={styles.answers}>
-          <div className='items'>{listItems}</div>
-          <li>{trivia[0].correct}</li>
+          <div>{listItems}</div>
+          <li onClick={answerHandler} className={styles.items}>
+            {correct}
+          </li>
         </div>
       </div>
     </div>
